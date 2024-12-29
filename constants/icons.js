b123a6177130a351a277d8df470b0f9e4eb418e1
Sudarshan Pokhrel <sudarshan.pokhrel2k62@gmail.com>
2024-12-29 21:35:01 +0545

    Add cart icon to constants and update TabLayout to include Cart tab

--- constants/icons.js
+++ constants/icons.js
@@ -11,6 +11,7 @@ import logout from "../assets/icons/logout.png";
 import eyeHide from "../assets/icons/eye-hide.png";
 import eye from "../assets/icons/eye.png";
 import play from "../assets/icons/play.png";
+import cart from "../assets/icons/trolley.png";
 
 export default {
   play,
